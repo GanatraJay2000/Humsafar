1db0477d1b2a0c458f0a66aa9d9096d0f2332b1d
GanatraJay2000 <jbg@somaiya.edu>
2020-07-03 18:42:08 +0530

    Layout Basic

--- header.php
+++ header.php
@@ -1,1 +1,32 @@
- <?php wp_head(); ?>
+<!DOCTYPE html>
+<html <?php language_attributes( ); ?>>
+
+<head>
+    <meta charset="<?php bloginfo( 'charset' ) ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo( 'name' ) ?></title>
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+    <!-- Site Header -->
+    <header class="site-header">
+        <h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+        <h5><?php bloginfo( 'description' ); ?></h5>
+
+        <nav class="site-nav">
+            <?php
+
+            $args = array(
+                'theme_location'=>'primary',
+            );
+
+            wp_nav_menu($args); 
+            
+            ?>
+        </nav>
+    </header>
+    <!-- Site Header -->
+
+    <div class="container">
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-Hi there, nice to work with you. Try Using Git Push !
+<!-- Hi there, nice to work with you. Try Using Git Push !
 FrontEnd and Dom people do tasks in their workspace . Backend people start work on the task given to you.
 Mostlikely there will be some redundant work because our database is not synced. I'm seeing what i can do.
 Hello Jay, This is Abhilash.
@@ -13,4 +13,21 @@ Hey, This is Sarthaki :)
 
 
 
-<?php echo "<br><br> As you try to try working on Workspace page it is showing inndex page only. this is a feature of wordpress. so for testing test it locally and then copy the file to the workspace directory.<br> You can copy the vendors folder locally. It contains nice libraries.";?>
+<?php echo "<br><br> As you try to try working on Workspace page it is showing inndex page only. this is a feature of wordpress. so for testing test it locally and then copy the file to the workspace directory.<br> You can copy the vendors folder locally. It contains nice libraries.";?> -->
+
+<?php get_header(); ?>
+
+<?php
+
+while(have_posts())
+{
+    the_post();?>
+<article class="post">
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <p><?php the_content(); ?></p>
+</article>
+<?php
+}
+?>
+
+<?php get_footer(); ?>
