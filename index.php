66fb4c69cd3e745413abe1be34990b67716e5531
GanatraJay2000 <jbg@somaiya.edu>
2020-07-03 09:44:47 +0530

    .

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 Hi there, nice to work with you. Try Using Git Push !
 FrontEnd and Dom people do tasks in their workspace . Backend people start work on the task given to you.
+Mostlikely there will be some redundant work because our database is not synced. I'm seeing what i can do.
