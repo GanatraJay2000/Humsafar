a0ce75d8de2eb64e863400cbffce1bee3d3d3b10
GanatraJay2000 <jbg@somaiya.edu>
2020-07-03 12:16:50 +0530

    Copy Space

--- index.php
+++ index.php
@@ -3,3 +3,8 @@ FrontEnd and Dom people do tasks in their workspace . Backend people start work 
 Mostlikely there will be some redundant work because our database is not synced. I'm seeing what i can do.
 Hello Jay, This is Abhilash.
 Hii, This is Vanshika.
+
+
+
+
+<?php echo "<br><br> As you try to try working on Workspace page it is showing inndex page only. this is a feature of wordpress. so for testing test it locally and then copy the file to the workspace directory.";
