9009c0594750416f05749ecf73f7ee3cc26f12cf
GanatraJay2000 <jbg@somaiya.edu>
2020-07-03 13:54:21 +0530

    Index.php

--- index.php
+++ index.php
@@ -3,6 +3,14 @@ FrontEnd and Dom people do tasks in their workspace . Backend people start work 
 Mostlikely there will be some redundant work because our database is not synced. I'm seeing what i can do.
 Hello Jay, This is Abhilash.
 Hii, This is Vanshika.
-<<<<<<< HEAD
-    <?php echo "<br><br> As you try to try working on Workspace page it is showing inndex page only. this is a feature of wordpress. so for testing test it locally and then copy the file to the workspace directory.<br> You can copy the vendors folder locally. It contains nice libraries.";?>=======HEllo
-    Everyone, This is Pallavi. Hi, This is Sarthaki :)>>>>>>> a637b476c2671c5e767a621df33764f54d109503
+=======HEllo
+Everyone, This is Pallavi.
+Hi, This is Sarthaki :)
+
+
+
+
+
+
+
+<?php echo "<br><br> As you try to try working on Workspace page it is showing inndex page only. this is a feature of wordpress. so for testing test it locally and then copy the file to the workspace directory.<br> You can copy the vendors folder locally. It contains nice libraries.";?>
