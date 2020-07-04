d839291decd7bef7d9e818ca44c7f18ce11b96aa
AbhilashReddyGone <36304287+AbhilashReddyGone@users.noreply.github.com>
2020-07-04 17:40:17 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -22,3 +22,24 @@ register_nav_menus( array(
     'primary_two'=>__( 'Primary Two Menu' ),
     'footer'=> __( 'Footer Menu' ),
 ) );
+
+// Our custom post type function
+function create_posttype() {
+ 
+    register_post_type( 'movies',
+    // CPT Options
+        array(
+            'labels' => array(
+                'name' => __( 'Movies' ),
+                'singular_name' => __( 'Movie' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'movies'),
+            'show_in_rest' => true,
+ 
+        )
+    );
+}
+// Hooking up our function to theme setup
+add_action( 'init', 'create_posttype' );
