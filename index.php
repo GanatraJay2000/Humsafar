d03cf200d46deb5d14f9bf589db8ededddac6db3
sarthakinaik <59866909+sarthakinaik@users.noreply.github.com>
2020-07-06 21:46:30 +0530

    Changes

--- index.php
+++ index.php
@@ -14,20 +14,30 @@ Hey, This is Sarthaki :)
 
 
 <?php echo "<br><br> As you try to try working on Workspace page it is showing inndex page only. this is a feature of wordpress. so for testing test it locally and then copy the file to the workspace directory.<br> You can copy the vendors folder locally. It contains nice libraries.";?> -->
-
-<?php get_header(); ?>
-
+<center>
 <?php
 
 while(have_posts())
 {
     the_post();?>
+	
+<?php get_header();
+get_post();
+$thumbnail = get_field('image');
+$names = get_field('names');
+$date = get_field('date'); ?>
+
+
+	
 <article class="post">
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <h2><a href="<?php the_permalink(); ?>"><?php echo $names; ?></a></h2>
+	 <h6><?php echo $date; ?></h6>
+			<img width="400px" src="<?php echo $thumbnail['url']; ?>" alt="<?php echo $thumbnail['alt']; ?>" />
+		
     <p><?php the_content(); ?></p>
 </article>
 <?php
 }
 ?>
-
+</center>
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,16 +1,28 @@
-<?php get_header(); ?>
-
+<center>
 <?php
 
 while(have_posts())
 {
     the_post();?>
+	
+<?php get_header();
+get_post();
+$thumbnail = get_field('image');
+$names = get_field('names');
+$date = get_field('date'); ?>
+
+
+	
 <article class="post">
-    <h2><?php the_title(); ?></h2>
+    <h2><a href="<?php the_permalink(); ?>"><?php echo $names; ?></a></h2>
+	 <h6><?php echo $date; ?></h6>
+			<img width="400px" src="<?php echo $thumbnail['url']; ?>" alt="<?php echo $thumbnail['alt']; ?>" />
+		
     <p><?php the_content(); ?></p>
 </article>
 <?php
 }
 ?>
+</center>
 
 <?php get_footer(); ?>
