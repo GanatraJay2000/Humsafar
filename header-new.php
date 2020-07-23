c7f08c6bd2a5171c7abbc86f8bd9c1b1ed718ca7
GanatraJay2000 <jbg@somaiya.edu>
2020-07-24 01:08:16 +0530

    book artist layout resp

--- header-new.php
+++ header-new.php
@@ -11,6 +11,20 @@
         html {
             margin: 0 !important;
         }
+#myModal{
+    min-height:20vh;
+}
+.modal {
+    position: absolute;
+    top: -21;
+    left: 0;
+    z-index: 1050;
+    display: none;
+    width: 100%;
+    height: 150%;
+    overflow: hidden;
+    outline: 0;
+}
     </style>
 </head>
 
@@ -89,8 +103,7 @@
             </div></a>
         </div>
     </header>
-    <!-- Site Header -->
-    <div data-dismiss="modal">
+    <!-- Site Header -->    
     <div id="myModal" class="modal fade book-artist-modal" role="dialog" >
         <div class="modal-dialog" >
             <div class="modal-content">
@@ -148,62 +161,8 @@
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+        </div>    
     <?php
 if(!is_page(7)) { ?><div class="content wrapper"><?php } ?>
     
 
-
-        <!-- 
-
-    <div class="mt-3 text-center">
-        <h1>Book Artist
-            <hr>
-        </h1>
-        <form action="" method="">
-            <div class="d-flex flex-wrap text-left px-5 mx-5">
-                <div class="form-group col-lg-6 col-12">
-                    <div class="form-group">Name<input type="text" required class="form-control" autocomplete="off" name="name">
-                    </div>
-                    <div class="form-group">Company Name<input type="text" required class="form-control" autocomplete="off"
-                            name="company_name">
-                    </div>
-                    <div class="form-group">Phone Number<input type="text" required class="form-control" autocomplete="off"
-                            name="phone_no">
-                    </div>
-                    <div class="form-group">Email Id<input type="text" required class="form-control" name="email_id"
-                            autocomplete="off">
-                    </div>
-                    <div class="form-group">Address<textarea rows="1" class="form-control" name="address"
-                            autocomplete="off"></textarea>
-                    </div>
-                </div>
-                <div class="form-group col-lg-6 col-12">
-                    <div class="form-group">
-                        Date of Event <input type="text" required type='text' name="date_of_event" autocomplete="off"
-                            class="form-control datepicker-here" data-position="top right"
-                            data-date-format="yyyy-mm-dd">
-                    </div>
-                    <div class="form-group">Event Venu/City<input type="text" required class="form-control" autocomplete="off"
-                            name="venue">
-                    </div>
-                    <div class="form-group">Event Type<input type="text" required class="form-control" autocomplete="off"
-                            name="event_type">
-                    </div>
-                    <div class="form-group">Artist Type<select class="form-control" autocomplete="off"
-                            name="artist_type"></select>
-                    </div>
-                    <div class="form-group">Artist Name<input type="text" required class="form-control" autocomplete="off"
-                            name="artist_name">
-                    </div>
-                </div>
-            </div>
-            <div>
-                <button type="submit" class="btn btn-dark" style="border-radius:0">Submit</button>
-            </div>
-        </form>
-    </div>
-
-
-     -->
