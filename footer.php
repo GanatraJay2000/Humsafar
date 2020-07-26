9eec50ed614bccda48fa6a87a217dbc50c1e5714
GanatraJay2000 <jbg@somaiya.edu>
2020-07-26 15:19:36 +0530

    bg-sec

--- footer.php
+++ footer.php
@@ -98,7 +98,7 @@ foreach ( $slide_2 as $image ) {?>
         </button>
                 <div class="modal-dialog" role="document">                
                     <div class="modal-content">       
-                        <div class="modal-header">
+                        <div class="modal-header bg-secondary">
                         <h5 class="modal-title">Subscribe to Newsletter</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
