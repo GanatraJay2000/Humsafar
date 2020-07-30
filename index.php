25d80a24dcfb68462dcf523a293d6155435bfc1b
GanatraJay2000 <jbg@somaiya.edu>
2020-07-30 14:37:04 +0530

    gaga

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <?php while(have_posts()) {
     the_post(); ?>
 <article class="post my-5">
-    <h1><?php the_title(); ?></h1>
+    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
     <p><?php the_content(); ?></p>    
 </article>
 <hr>
--- single.php
+++ single.php
@@ -1,40 +1,14 @@
-<center>
-<?php
+<?php get_header(); ?>
 
-while(have_posts())
-{
-    the_post();?>
-	
-<?php get_header();
-get_post();
- $thumbnail_image = rwmb_meta('thumbnail_image', array( 'size' => 'medium' ) );
- $gallery = rwmb_meta('gallery', array( 'size' => 'medium' ) );
- ?>
-
-
-	
-<article class="post">
-    <h2><?php the_title(); ?></h2>
-	 <h6><?php echo $date; ?></h6>
-			<!-- <img width="400px" src="<?php echo $thumbnail['url']; ?>" alt="<?php echo $thumbnail['alt']; ?>" /> -->
-	<?php foreach ( $thumbnail_image as $image ) {?>
-        <img src="<?php echo $image['url'] ?>" alt="">
-    <?php } ?>
-    <p><?php the_content(); ?></p>
-    <p><?php echo rwmb_meta('event_date'); ?></p>
-    <p><?php echo rwmb_meta('location'); ?></p>
-    <?php foreach ( $gallery as $image ) {?>
-        <img src="<?php echo $image['url'] ?>" alt="">
-    <?php } ?>
-
+<?php while(have_posts()) {
+    the_post(); ?>
+<article class="post my-5">
+    <h1><?php the_title(); ?></h1>
+    <p><?php the_content(); ?></p>    
 </article>
+<hr>
 <?php
 }
 ?>
-</center>
-
+                  
 <?php get_footer(); ?>
-
-
-
-
