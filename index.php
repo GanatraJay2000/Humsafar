0d834bb2f78b693effa4fcf1bc23c1c31cb23a5e
GanatraJay2000 <jbg@somaiya.edu>
2020-07-30 19:05:37 +0530

    het yhere

--- index.php
+++ index.php
@@ -2,11 +2,10 @@
 <center><h1>Blog</h1></center>
 <?php while(have_posts()) {
     the_post(); ?>
-<article class="post my-5">
+<article class="post">
     <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
     <p><?php echo wp_trim_words(get_the_content(), 25); ?></p>   
 </article>
-<hr>
 <?php
 }
 ?>
