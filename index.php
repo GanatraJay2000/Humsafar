68cfdaceb62fdb75a514110eccef095173381fda
GanatraJay2000 <jbg@somaiya.edu>
2020-07-30 14:33:54 +0530

    haha

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
-hey there
+<center><h1>Blog</h1></center>
+<?php while(have_posts()) {
+    the_post(); ?>
+<article class="post my-5">
+    <h1><?php the_title(); ?></h1>
+    <p><?php the_content(); ?></p>    
+</article>
+<hr>
+<?php
+}
+?>
+                  
 <?php get_footer(); ?>
