422f22ebb6b4e6240172fe9e7e8bb78dff4f13ab
GanatraJay2000 <jbg@somaiya.edu>
2020-07-31 03:40:47 +0530

    he he

--- header.php
+++ header.php
@@ -30,14 +30,14 @@
             ?>
             </nav>
 
-            <div class="nav-brand " style="border:2px solid gold;width:fit-content;margin:0 auto">
+            <div class="nav-brand " style="border:0px solid gold;width:fit-content;margin:0 auto">
                 <a class="" href="<?php echo site_url('weddings'); ?>">
                 <div class="d-flex justify-content-evenly align-items-center">
-                <div class="brand-logo" style="border:2px solid coral;">
-                    <img style="border:2px solid coral; " src="<?php echo get_template_directory_uri() . '/icon.png' ?>" alt="">
+                <div class="brand-logo" style="border:0px solid coral;">
+                    <img style="border:0px solid coral; " src="<?php echo get_template_directory_uri() . '/icon.png' ?>" alt="">
                     </div>
-                    <div class="brand-title" style="border:2px solid lightgreen;">
-                        <img style="border:2px solid teal;" src="<?php echo get_template_directory_uri() . '/title.png' ?>" alt="">
+                    <div class="brand-title" style="border:0px solid lightgreen;">
+                        <img style="border:0px solid teal;" src="<?php echo get_template_directory_uri() . '/title.png' ?>" alt="">
                     </div>
                     </div>
                 </a>
