d6cf5e7418c2d37351c08cbd1449a06f0aaae293
GanatraJay2000 <jbg@somaiya.edu>
2020-07-30 23:42:24 +0530

    hey there

--- page-india.php
+++ page-india.php
@@ -428,7 +428,7 @@ foreach ( $image_on_destination_page as $image ) {?>
 </a>
 <?php $i++; } ?>
 </section>
-<center><button style="background-color:var(--primary-color) !important;color:white;" class="btn mt-5">Back to Destinations</button></center>
+<center><button style="background-color:#ecc86b !important;color:white;" class="btn mt-5">Back to Destinations</button></center>
 </section>
     </div>
 
