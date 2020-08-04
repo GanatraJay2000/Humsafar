cdc40cd30d7279da7e25a1c90674a770c6885d49
GanatraJay2000 <jbg@somaiya.edu>
2020-08-04 20:06:44 +0530

    jijijiji

--- page-about-us.php
+++ page-about-us.php
@@ -260,7 +260,7 @@
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis dignissimos aperiam labore consectetur quaerat non magnam voluptas, perferendis ea libero veniam facilis autem impedit dolor. Accusantium quia optio et aliquid?
     </div>
     <div class="card-deck my-5">
-  <div class="card">
+  <div class="card mb-4">
     <img class="card-img-top" style="width:100px;margin:30px auto;" src="<?php echo get_template_directory_uri() . '/assets_inside/crown.png' ?>" alt="Card image cap">    
     <div class="card-body  text-center">
       <h5 class="card-title">Fairytale</h5>
@@ -271,7 +271,9 @@ We love weaving your emotions and personality into a wedding.<br><br>
 The fairytale that you’ve always dreamt of, no matter how crazy or simple, has to be designed and put into a viable plan. We make sure everything about your fairytale screams you!</p>      
     </div>
   </div>
-  <div class="card">
+  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
+  <div class="card mb-4">
+  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
   <img class="card-img-top" style="width:100px;margin:30px auto;" src="<?php echo get_template_directory_uri() . '/assets_inside/respect.png' ?>" alt="Card image cap">    
     <div class="card-body  text-center">
       <h5 class="card-title">Fairtale</h5>
@@ -282,7 +284,8 @@ We understand that the hard-earned money being spent on such important days need
 Working budget backwards ensures we plan and design a wedding suited to your requirements.</p>      
     </div>
   </div>
-  <div class="card">
+  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
+  <div class="card mb-4">
   <img class="card-img-top" style="width:100px;margin:30px auto;" src="<?php echo get_template_directory_uri() . '/assets_inside/wand.png' ?>" alt="Card image cap">  
     <div class="card-body  text-center">
       <h5 class="card-title">Flairtale</h5>
@@ -309,6 +312,7 @@ The priority here is to run the actual show seamlessly with adherence to timelin
       <p class="card-text">During this phase we meet, greet and get to know the style and vibe of our bride and groom. We get down-to-details like the budget and guest list to make sure we’re aligned for the requirements of your big day. Then, we collaborate, concept and start creating a vision. Have you always dreamed of a small, intimate backyard party or are you more of a ballroom, fairytale, where’s-my-glass slipper kind of a girl? Deciding on your dream direction allows us to choose vendors and plot key milestones to make your wedding day a reality. </p>      
     </div>
   </div>
+  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
   <div class="card">
     <img class="card-img-top" style="width:160px;margin:20px auto;"  src="https://images.squarespace-cdn.com/content/v1/5c4111e5b98a78c514801cef/1558377805267-2QR1PJ7QRQI9WOS7A52N/ke17ZwdGBToddI8pDm48kPmrXPVZ9IZXdToIOIR3bYpZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7coMdWJheMWZGj8ZBPnfUaPSgvGrwmH9g7Am67QkkDgnFR0Pz3fYPXtHzuPoZ7T36Q/smallicons-10.png?format=300w" alt="Card image cap">
     <div class="card-body">
@@ -318,13 +322,14 @@ The priority here is to run the actual show seamlessly with adherence to timelin
 </p>      
     </div>
   </div>
+  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
   <div class="card">
     <img class="card-img-top" style="width:160px;margin:20px auto;"  src="https://images.squarespace-cdn.com/content/v1/5c4111e5b98a78c514801cef/1558377895913-ET0QUOVDVBWTB9GZZSK8/ke17ZwdGBToddI8pDm48kPmrXPVZ9IZXdToIOIR3bYpZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7coMdWJheMWZGj8ZBPnfUaPSgvGrwmH9g7Am67QkkDgnFR0Pz3fYPXtHzuPoZ7T36Q/smallicons-12.png?format=300w" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title text-center">Action Phase</h5>
       <p class="card-text">Now, the planning pays off and your vision comes to life. Your joy takes center stage while our team carefully nuances every detail and manages any obstacle. From red wine stains to sick officiants, we take over so you and your family can embrace every moment of your big day.</p>      
     </div>
-  </div>
+  </div>  
 </div>
     </div>
     <div class="meet-team">
@@ -351,6 +356,7 @@ The priority here is to run the actual show seamlessly with adherence to timelin
         </a>
     </div>
   </div>
+  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
   <div class="card">
   <!-- <img src="<?php echo get_template_directory_uri() . '/assets_inside/ritika.png'; ?>" class="card-img-top" alt="..."> -->
   <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAALoAAAC6CAMAAAAu0KfDAAAAA1BMVEWcnpvbbJEWAAAAOElEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALwZh94AAaYbEC4AAAAASUVORK5CYII=" class="card-img-top" alt="...">
@@ -369,6 +375,7 @@ The priority here is to run the actual show seamlessly with adherence to timelin
         </a>
     </div>
   </div>
+  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
   <div class="card">
   <!-- <img src="<?php echo get_template_directory_uri() . '/assets_inside/chetan.png'; ?>" class="card-img-top" alt="..."> -->
   <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAALoAAAC6CAMAAAAu0KfDAAAAA1BMVEWcnpvbbJEWAAAAOElEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALwZh94AAaYbEC4AAAAASUVORK5CYII=" class="card-img-top" alt="...">
