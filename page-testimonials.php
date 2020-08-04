1d8f45ef47bde8fbbbfa3f342d1cbf8f5525403c
AbhilashReddyGone <36304287+AbhilashReddyGone@users.noreply.github.com>
2020-08-04 20:50:38 +0530

    Update page-testimonials.php

--- page-testimonials.php
+++ page-testimonials.php
@@ -1,3 +1,117 @@
 <?php get_header(); ?>
 Testimonials
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="StyleSheet3.css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Playfair+Display" />
+</head>
+<body>
+    <div class="container">
+        <div class="box">
+            <div class="row">
+                <div class="col-md-1">
+                </div>
+
+                <div class="col-md-4">
+                    <h1><i>Racheal&Dean</i></h1>
+                    <br /><br />
+
+                    <p class="para">
+                        "I'm a paragraph. Click here to add your own text and edit me. It’s easy.
+                        Just click “Edit Text” or double click me to add your own content and make changes to the font.
+                        I’m a great place for you to tell a story and let your users know a little more about you."
+                    </p>
+
+                </div>
+                <div class="col-md-1">
+
+                </div>
+                <div class="col-md-6">
+                    <img src="https://static.wixstatic.com/media/f61af8_fe2c9bf34b1743e7959b3e12c484f70b~mv2_d_2600_1733_s_2.jpg/v1/fill/w_560,h_400,al_c,q_80,usm_0.66_1.00_0.01/f61af8_fe2c9bf34b1743e7959b3e12c484f70b~mv2_d_2600_1733_s_2.webp">
+                </div>
+            </div>
+            <
+        </div>
+        <div class="box">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="https://static.wixstatic.com/media/f61af8_4b3fb632f34b4e128631714ce1208176~mv2_d_3452_2554_s_4_2.jpg/v1/fill/w_560,h_400,al_c,q_80,usm_0.66_1.00_0.01/f61af8_4b3fb632f34b4e128631714ce1208176~mv2_d_3452_2554_s_4_2.webp">
+                </div>
+                <div class="col-md-1">
+                </div>
+
+                <div class="col-md-4">
+                    <h1><i>Cristina & Remo</i></h1>
+                    <br /><br />
+
+                    <p class="para">
+                        "I'm a paragraph. Click here to add your own text and edit me. It’s easy.
+                        Just click “Edit Text” or double click me to add your own content and make changes to the font.
+                        I’m a great place for you to tell a story and let your users know a little more about you."
+                    </p>
+
+                </div>
+                <div class="col-md-1">
+
+                </div>
+
+            </div>
+        </div>
+        <div class="box">
+            <div class="row">
+                <div class="col-md-1">
+                </div>
+
+                <div class="col-md-4">
+                    <h1><i>Shauna & Ali</i></h1>
+                    <br /><br />
+
+                    <p class="para">
+                        "I'm a paragraph. Click here to add your own text and edit me. It’s easy.
+                        Just click “Edit Text” or double click me to add your own content and make changes to the font.
+                        I’m a great place for you to tell a story and let your users know a little more about you."
+                    </p>
+
+                </div>
+                <div class="col-md-1">
+
+                </div>
+                <div class="col-md-6">
+                    <img src="https://static.wixstatic.com/media/f61af8_c9cad4ee33364cffaf779ec593175b9f~mv2_d_4381_2977_s_4_2.jpg/v1/fill/w_560,h_400,al_c,q_80,usm_0.66_1.00_0.01/f61af8_c9cad4ee33364cffaf779ec593175b9f~mv2_d_4381_2977_s_4_2.webp">
+                </div>
+            </div>
+            <
+        </div>
+        <div class="box">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="https://static.wixstatic.com/media/f61af8_82e45530c75e462f9964bcab7aaede5f~mv2_d_4202_2791_s_4_2.jpg/v1/fill/w_560,h_400,al_c,q_80,usm_0.66_1.00_0.01/f61af8_82e45530c75e462f9964bcab7aaede5f~mv2_d_4202_2791_s_4_2.webp">
+                </div>
+                <div class="col-md-1">
+                </div>
+
+                <div class="col-md-4">
+                    <h1><i>Kim & Andy</i></h1>
+                    <br /><br />
+
+                    <p class="para">
+                        "I'm a paragraph. Click here to add your own text and edit me. It’s easy.
+                        Just click “Edit Text” or double click me to add your own content and make changes to the font.
+                        I’m a great place for you to tell a story and let your users know a little more about you."
+                    </p>
+
+                </div>
+                <div class="col-md-1">
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>-
 <?php get_footer(); ?>
