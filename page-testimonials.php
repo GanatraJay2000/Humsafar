3ef2c197c1e547c075bf46b8a1b4df7443346832
GanatraJay2000 <jbg@somaiya.edu>
2020-08-06 01:26:09 +0530

    hi there

--- page-testimonials.php
+++ page-testimonials.php
@@ -1,120 +1,125 @@
-<?php get_header() ?>
+<?php get_header(); ?>
+<style>
+    .container {
+        padding-top: 40px;
+    }
 
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta charset="utf-8" />
-    <title></title>
+    .title {
+        font-family: 'Times New Roman';
+        font-size: 30px;
+    }
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="StyleSheet3.css">
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Playfair+Display" />
-</head>
-<body>
-    <div class="container">
-        <div class="box">
-            <div class="row">
-                <div class="col-md-1">
+    .title::first-letter {
+        font-family: 'Times New Roman';
+        font-size: 100px;
+    }
+
+    .box {
+        padding-bottom: 150px;
+    }
+
+    .para p{
+        font-family: "Myriad Pro", sans-serif;
+        color: #888;
+        line-height: 30px;
+        text-align: justify;
+    }
+
+    .first-letter {
+        font-family: 'Times New Roman';
+        font-size: 100px;
+    }
+
+
+    .image {
+        position: relative;
+        width: 400px;
+        height: 400px;
+    }
+
+
+
+    .image img {        
+        height: 100%;
+        position: relative;        
+    }
+
+
+    .flex-row .image::after {
+        content: "";
+        display: block;
+        height: 100%;
+        width: 140%;
+        z-index: -1;
+        position: absolute;
+        top: -60px;
+        right: -240px;
+        background-color: #f7efe9;
+    }
+
+    .flex-row-reverse .image::after {
+        content: "";
+        display: block;
+        height: 100%;
+        width: 140%;
+        z-index: -1;
+        position: absolute;
+        top: -60px;
+        left: -80px;
+        background-color: #f7efe9;
+    }
+</style>
+<div class="container">
+    <br />
+    <br /><br />
+     <?php
+$events = new WP_Query(array(
+    'posts_per_page' => 2,
+    'post_type'=>'events',
+    'meta_key' => 'event_date',
+    'orderby' => 'meta_value_num',
+    'order' => 'DESC',
+));
+$i=0;
+while($events->have_posts())
+{
+    $events->the_post();             
+    $thumbnail_image = rwmb_meta('thumbnail_image', array( 'size' => 'large' ) );
+    ?>
+    
+    <div class="box">
+        <div class="row d-flex flex-row<?php 
+        if($i%2==1){
+            echo "-reverse";
+        }
+        ?> justify-content-between">
+
+            <div class="col-md-6 col-lg-4 col-xs-4 col-md-4">
+                <div class="title">
+                    <p>
+                        <i><?php the_title(); ?></i>
+                        <p>
                 </div>
 
-                <div class="col-md-4">
-                    <div class="heading">
-                        <p><span class="first-letter"><i>R</i></span><i>achel & Dean</i><p>
-                    </div>
-
-                    <p class="para">
-                        "I'm a paragraph. Click here to add your own text and edit me. It’s easy.
-                        Just click “Edit Text” or double click me to add your own content and make changes to the font.
-                        I’m a great place for you to tell a story and let your users know a little more about you."
-                    </p>
-
-                </div>
-                <div class="col-md-1">
-
-                </div>
-                <div class="col-md-6">
-                    <img src="https://static.wixstatic.com/media/f61af8_fe2c9bf34b1743e7959b3e12c484f70b~mv2_d_2600_1733_s_2.jpg/v1/fill/w_560,h_400,al_c,q_80,usm_0.66_1.00_0.01/f61af8_fe2c9bf34b1743e7959b3e12c484f70b~mv2_d_2600_1733_s_2.webp">
+                <div class="para">
+                   <?php the_excerpt(); ?>
                 </div>
             </div>
-        
-        </div>
-        <div class="box">
-            <div class="row">
-                <div class="col-md-6">
-                    <img src="https://static.wixstatic.com/media/f61af8_4b3fb632f34b4e128631714ce1208176~mv2_d_3452_2554_s_4_2.jpg/v1/fill/w_560,h_400,al_c,q_80,usm_0.66_1.00_0.01/f61af8_4b3fb632f34b4e128631714ce1208176~mv2_d_3452_2554_s_4_2.webp">
+
+            <div class="col-md-6 col-lg-6 col-xs-6 col-md-6">
+                <div class="image">
+                     <?php foreach ( $thumbnail_image as $image ) {?>
+           <img src="<?php echo $image['url'] ?>" alt="">
+        <?php } ?>
+                    <br /><br />
                 </div>
-                <div class="col-md-1">
-                </div>
-
-                <div class="col-md-4">
-                    <div class="heading">
-                        <p><span class="first-letter"><i>C</i></span><i>ristina & Remo</i><p>
-                    </div>
- 
-                        <p class="para">
-                            "I'm a paragraph. Click here to add your own text and edit me. It’s easy.
-                            Just click “Edit Text” or double click me to add your own content and make changes to the font.
-                            I’m a great place for you to tell a story and let your users know a little more about you."
-                        </p>
-
-                    </div>
-                    <div class="col-md-1">
-
-                    </div>
-
-                </div>
-        </div>
-        <div class="box">
-            <div class="row">
-                <div class="col-md-1">
-                </div>
-
-                <div class="col-md-4">
-                    <div class="heading">
-                        <p><span class="first-letter"><i>S</i></span><i>hauna & Ali</i><p>
-                    </div>
-
-                        <p class="para">
-                            "I'm a paragraph. Click here to add your own text and edit me. It’s easy.
-                            Just click “Edit Text” or double click me to add your own content and make changes to the font.
-                            I’m a great place for you to tell a story and let your users know a little more about you."
-                        </p>
-
-                    </div>
-                    <div class="col-md-1">
-
-                    </div>
-                    <div class="col-md-6">
-                        <img src="https://static.wixstatic.com/media/f61af8_c9cad4ee33364cffaf779ec593175b9f~mv2_d_4381_2977_s_4_2.jpg/v1/fill/w_560,h_400,al_c,q_80,usm_0.66_1.00_0.01/f61af8_c9cad4ee33364cffaf779ec593175b9f~mv2_d_4381_2977_s_4_2.webp">
-                    </div>
-                </div>
-        </div>
-        <div class="box">
-            <div class="row">
-                <div class="col-md-6">
-                    <img src="https://static.wixstatic.com/media/f61af8_82e45530c75e462f9964bcab7aaede5f~mv2_d_4202_2791_s_4_2.jpg/v1/fill/w_560,h_400,al_c,q_80,usm_0.66_1.00_0.01/f61af8_82e45530c75e462f9964bcab7aaede5f~mv2_d_4202_2791_s_4_2.webp">
-                </div>
-                <div class="col-md-1">
-                </div>
-
-                <div class="col-md-4">
-                    <div class="heading">
-                        <p><span class="first-letter"><i>K</i></span><i>im & Andy</i><p>
-                    </div>
-
-                        <p class="para">
-                            "I'm a paragraph. Click here to add your own text and edit me. It’s easy.
-                            Just click “Edit Text” or double click me to add your own content and make changes to the font.
-                            I’m a great place for you to tell a story and let your users know a little more about you."
-                        </p>
-
-                    </div>
-                    <div class="col-md-1">
-
-                    </div>
-
-                </div>
+            </div>
         </div>
     </div>
-</body>
-</html>-
-<?php get_footer() ?>
+    
+    <?php
+    $i++;
+}      ?>
+    <br><br><br>       
+</div>
+<?php get_footer(); ?>
