8d6fd35b7ed6dfd7baace0dbc9ca777d9d10ed37
AbhilashReddyGone <36304287+AbhilashReddyGone@users.noreply.github.com>
2020-08-05 13:22:18 +0530

    Update page-testimonials.php

--- page-testimonials.php
+++ page-testimonials.php
@@ -1,5 +1,5 @@
-<?php get_header(); ?>
-Testimonials
+<!DOCTYPE html>
+
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
@@ -17,8 +17,9 @@ Testimonials
                 </div>
 
                 <div class="col-md-4">
-                    <h1><i>Racheal&Dean</i></h1>
-                    <br /><br />
+                    <div class="heading">
+                        <p><span class="first-letter"><i>R</i></span><i>achel & Dean</i><p>
+                    </div>
 
                     <p class="para">
                         "I'm a paragraph. Click here to add your own text and edit me. It’s easy.
@@ -34,7 +35,7 @@ Testimonials
                     <img src="https://static.wixstatic.com/media/f61af8_fe2c9bf34b1743e7959b3e12c484f70b~mv2_d_2600_1733_s_2.jpg/v1/fill/w_560,h_400,al_c,q_80,usm_0.66_1.00_0.01/f61af8_fe2c9bf34b1743e7959b3e12c484f70b~mv2_d_2600_1733_s_2.webp">
                 </div>
             </div>
-            <
+        
         </div>
         <div class="box">
             <div class="row">
@@ -45,21 +46,22 @@ Testimonials
                 </div>
 
                 <div class="col-md-4">
-                    <h1><i>Cristina & Remo</i></h1>
-                    <br /><br />
+                    <div class="heading">
+                        <p><span class="first-letter"><i>C</i></span><i>ristina & Remo</i><p>
+                    </div>
+ 
+                        <p class="para">
+                            "I'm a paragraph. Click here to add your own text and edit me. It’s easy.
+                            Just click “Edit Text” or double click me to add your own content and make changes to the font.
+                            I’m a great place for you to tell a story and let your users know a little more about you."
+                        </p>
 
-                    <p class="para">
-                        "I'm a paragraph. Click here to add your own text and edit me. It’s easy.
-                        Just click “Edit Text” or double click me to add your own content and make changes to the font.
-                        I’m a great place for you to tell a story and let your users know a little more about you."
-                    </p>
+                    </div>
+                    <div class="col-md-1">
+
+                    </div>
 
                 </div>
-                <div class="col-md-1">
-
-                </div>
-
-            </div>
         </div>
         <div class="box">
             <div class="row">
@@ -67,24 +69,24 @@ Testimonials
                 </div>
 
                 <div class="col-md-4">
-                    <h1><i>Shauna & Ali</i></h1>
-                    <br /><br />
+                    <div class="heading">
+                        <p><span class="first-letter"><i>S</i></span><i>hauna & Ali</i><p>
+                    </div>
 
-                    <p class="para">
-                        "I'm a paragraph. Click here to add your own text and edit me. It’s easy.
-                        Just click “Edit Text” or double click me to add your own content and make changes to the font.
-                        I’m a great place for you to tell a story and let your users know a little more about you."
-                    </p>
+                        <p class="para">
+                            "I'm a paragraph. Click here to add your own text and edit me. It’s easy.
+                            Just click “Edit Text” or double click me to add your own content and make changes to the font.
+                            I’m a great place for you to tell a story and let your users know a little more about you."
+                        </p>
 
-                </div>
-                <div class="col-md-1">
+                    </div>
+                    <div class="col-md-1">
 
+                    </div>
+                    <div class="col-md-6">
+                        <img src="https://static.wixstatic.com/media/f61af8_c9cad4ee33364cffaf779ec593175b9f~mv2_d_4381_2977_s_4_2.jpg/v1/fill/w_560,h_400,al_c,q_80,usm_0.66_1.00_0.01/f61af8_c9cad4ee33364cffaf779ec593175b9f~mv2_d_4381_2977_s_4_2.webp">
+                    </div>
                 </div>
-                <div class="col-md-6">
-                    <img src="https://static.wixstatic.com/media/f61af8_c9cad4ee33364cffaf779ec593175b9f~mv2_d_4381_2977_s_4_2.jpg/v1/fill/w_560,h_400,al_c,q_80,usm_0.66_1.00_0.01/f61af8_c9cad4ee33364cffaf779ec593175b9f~mv2_d_4381_2977_s_4_2.webp">
-                </div>
-            </div>
-            <
         </div>
         <div class="box">
             <div class="row">
@@ -95,23 +97,23 @@ Testimonials
                 </div>
 
                 <div class="col-md-4">
-                    <h1><i>Kim & Andy</i></h1>
-                    <br /><br />
+                    <div class="heading">
+                        <p><span class="first-letter"><i>K</i></span><i>im & Andy</i><p>
+                    </div>
 
-                    <p class="para">
-                        "I'm a paragraph. Click here to add your own text and edit me. It’s easy.
-                        Just click “Edit Text” or double click me to add your own content and make changes to the font.
-                        I’m a great place for you to tell a story and let your users know a little more about you."
-                    </p>
+                        <p class="para">
+                            "I'm a paragraph. Click here to add your own text and edit me. It’s easy.
+                            Just click “Edit Text” or double click me to add your own content and make changes to the font.
+                            I’m a great place for you to tell a story and let your users know a little more about you."
+                        </p>
+
+                    </div>
+                    <div class="col-md-1">
+
+                    </div>
 
                 </div>
-                <div class="col-md-1">
-
-                </div>
-
-            </div>
         </div>
     </div>
 </body>
 </html>-
-<?php get_footer(); ?>
