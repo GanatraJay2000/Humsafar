84a66d4670391ed065d48c2c0ffbccd10189c96f
sarthakinaik <59866909+sarthakinaik@users.noreply.github.com>
2020-08-07 14:44:29 +0530

    ..

--- page-singledest.php
+++ page-singledest.php
@@ -148,7 +148,7 @@ h2,h4{
 <div class="row" >
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 <?php
-echo do_shortcode('[smartslider3 slider="5"]');
+echo do_shortcode('[smartslider3 slider="3"]');
 ?>
 </div>
 </div>
