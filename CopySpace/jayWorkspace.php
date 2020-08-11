29c93f1df36510b7dacbbe5b697c602f727439db
GanatraJay2000 <jbg@somaiya.edu>
2020-08-11 17:58:11 +0530

    hooho

--- CopySpace/jayWorkspace.php
+++ CopySpace/jayWorkspace.php
@@ -1,82 +1,81 @@
-<?php get_header(); ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Clients Hover</title>
-    <style>
-        .carousel {
-            /* background-color: #ffffe0 !important; */
-            background-color: #deffe0 !important;
-            border-radius: 5px;
-            height: 200px;
-            display: flex !important;
-            align-items: center;
-        }
-
-        .block {
-            height: 150px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-
-            position: relative;
-        }
-
-        .block img {
-            transition: transform ease 0.38s;
-        }
-
-        .block::after {
-            position: absolute;
-            left: 50%;
-            bottom: 10px;
-            content: '';
-            background: #fff;
-            height: 2.5px;
-            border-radius: 2px;
-            width: 0%;
-            transition: width ease 0.28s, left ease 0.28s;
-        }
-
-        .block:hover img {
-            transform: translate(0px, -10px);
-        }
-
-        .block:hover::after {
-            left: 40%;
-            width: 20%;
-        }
-    </style>
-</head>
-
-<body>
-    <h1 class="animate__animated animate__slideInDown">An animated element</h1>
-    <div class=" my-5 text-center">
-        <div class="carousel owl-carousel owl-theme text-dark">
-            <div class="block">
-                <img src="http://efactor4u.com/efactor/wordpress/wp-content/uploads/2017/08/balloon-safari-logo-2.png"
-                    alt="">
-            </div>
-            <div class="block"><img
-                    src="http://efactor4u.com/efactor/wordpress/wp-content/uploads/2017/09/pushkar-mela-logo-4.png"
-                    alt=""></div>
-            <div class="block"><img
-                    src="http://efactor4u.com/efactor/wordpress/wp-content/uploads/2017/09/icwf-logo-2.png" alt="">
-            </div>
-            <div class="block">
-                <img src="http://efactor4u.com/wp-content/uploads/2020/01/CBL-logo-New.png" alt="">
-            </div>
-            <div class="block">
-                <img src="http://efactor4u.com/wp-content/uploads/2020/01/SKYWALTZ-Sailing-New.png" alt="">
-            </div>
-        </div>
+<div class="heading">
+        <p>How We are Different</p>
+        <h1><span>*</span></h1>
     </div>
-</body>
+    <div class="how-diff">
+    <div class="how-diff-text">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis dignissimos aperiam labore consectetur quaerat non magnam voluptas, perferendis ea libero veniam facilis autem impedit dolor. Accusantium quia optio et aliquid?
+    </div>
+    <div class="card-deck mt-5">
+  <div class="card mb-4">
+    <img class="card-img-top" style="width:100px;margin:30px auto;" src="<?php echo get_template_directory_uri() . '/assets_inside/crown.png' ?>" alt="Card image cap">    
+    <div class="card-body  text-center">
+      <h5 class="card-title">Fairytale</h5>
+      <p class="card-text">Bringing your wedding vision to life!<br><br>
 
-</html>
+We love weaving your emotions and personality into a wedding.<br><br>
 
-<?php get_footer(); ?>
+The fairytale that you’ve always dreamt of, no matter how crazy or simple, has to be designed and put into a viable plan. We make sure everything about your fairytale screams you!</p>      
+    </div>
+  </div>
+  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
+  <div class="card mb-4">
+  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
+  <img class="card-img-top" style="width:100px;margin:30px auto;" src="<?php echo get_template_directory_uri() . '/assets_inside/respect.png' ?>" alt="Card image cap">    
+    <div class="card-body  text-center">
+      <h5 class="card-title">Fairtale</h5>
+      <p class="card-text">Constructing the Fairy Tale within a budget.<br><br>
+
+We understand that the hard-earned money being spent on such important days needs to be justified, without compromising on your wedding vision.<br><br>
+
+Working budget backwards ensures we plan and design a wedding suited to your requirements.</p>      
+    </div>
+  </div>
+  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
+  <div class="card mb-4">
+  <img class="card-img-top" style="width:100px;margin:30px auto;" src="<?php echo get_template_directory_uri() . '/assets_inside/wand.png' ?>" alt="Card image cap">  
+    <div class="card-body  text-center">
+      <h5 class="card-title">Flairtale</h5>
+      <p class="card-text">Making the dream plan come live into action!<br><br>
+
+The process of planning now comes to life and each and every aspect of making the wedding happen- and happen right- is taken care of by Team B Inc.<br><br>
+
+The priority here is to run the actual show seamlessly with adherence to timelines & the most painful attention to the finest details.</p>      
+    </div>
+  </div>
+</div>
+    </div>
+
+    <div class="planning">
+    <div class="heading">
+        <p>Phases</p>
+        <h1><span>*</span></h1>
+    </div>
+    <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" style="width:160px;margin:20px auto;"  src="https://images.squarespace-cdn.com/content/v1/5c4111e5b98a78c514801cef/1558377875221-39W4PF09WKNKL0OH8INA/ke17ZwdGBToddI8pDm48kPmrXPVZ9IZXdToIOIR3bYpZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7coMdWJheMWZGj8ZBPnfUaPSgvGrwmH9g7Am67QkkDgnFR0Pz3fYPXtHzuPoZ7T36Q/smallicons-11.png?format=300w" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title text-center">Planning Phase</h5>
+      <p class="card-text">During this phase we meet, greet and get to know the style and vibe of our bride and groom. We get down-to-details like the budget and guest list to make sure we’re aligned for the requirements of your big day. Then, we collaborate, concept and start creating a vision. Have you always dreamed of a small, intimate backyard party or are you more of a ballroom, fairytale, where’s-my-glass slipper kind of a girl? Deciding on your dream direction allows us to choose vendors and plot key milestones to make your wedding day a reality. </p>      
+    </div>
+  </div>
+  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
+  <div class="card">
+    <img class="card-img-top" style="width:160px;margin:20px auto;"  src="https://images.squarespace-cdn.com/content/v1/5c4111e5b98a78c514801cef/1558377805267-2QR1PJ7QRQI9WOS7A52N/ke17ZwdGBToddI8pDm48kPmrXPVZ9IZXdToIOIR3bYpZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7coMdWJheMWZGj8ZBPnfUaPSgvGrwmH9g7Am67QkkDgnFR0Pz3fYPXtHzuPoZ7T36Q/smallicons-10.png?format=300w" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title text-center">Design Phase</h5>
+      <p class="card-text">TWith the big picture nailed down in phase one, now’s the time we bring your style to life. We create a custom design proposal encompassing everything from your cake to invitations to unexpected centerpieces that will have your guests Instagramming every last thoughtfully curated detail. We’ll handle sourcing and vendor communication so you can enjoy the surprise of “reveal day” when we share mockups of your entire design suite.
+
+</p>      
+    </div>
+  </div>
+  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
+  <div class="card">
+    <img class="card-img-top" style="width:160px;margin:20px auto;"  src="https://images.squarespace-cdn.com/content/v1/5c4111e5b98a78c514801cef/1558377895913-ET0QUOVDVBWTB9GZZSK8/ke17ZwdGBToddI8pDm48kPmrXPVZ9IZXdToIOIR3bYpZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7coMdWJheMWZGj8ZBPnfUaPSgvGrwmH9g7Am67QkkDgnFR0Pz3fYPXtHzuPoZ7T36Q/smallicons-12.png?format=300w" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title text-center">Action Phase</h5>
+      <p class="card-text">Now, the planning pays off and your vision comes to life. Your joy takes center stage while our team carefully nuances every detail and manages any obstacle. From red wine stains to sick officiants, we take over so you and your family can embrace every moment of your big day.</p>      
+    </div>
+  </div>  
+</div>
+    </div>
--- page-about-us.php
+++ page-about-us.php
@@ -259,87 +259,7 @@
             </div>
         </div>    
     </div>
-    <div class="heading">
-        <p>How We are Different</p>
-        <h1><span>*</span></h1>
-    </div>
-    <div class="how-diff">
-    <div class="how-diff-text">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis dignissimos aperiam labore consectetur quaerat non magnam voluptas, perferendis ea libero veniam facilis autem impedit dolor. Accusantium quia optio et aliquid?
-    </div>
-    <div class="card-deck mt-5">
-  <div class="card mb-4">
-    <img class="card-img-top" style="width:100px;margin:30px auto;" src="<?php echo get_template_directory_uri() . '/assets_inside/crown.png' ?>" alt="Card image cap">    
-    <div class="card-body  text-center">
-      <h5 class="card-title">Fairytale</h5>
-      <p class="card-text">Bringing your wedding vision to life!<br><br>
-
-We love weaving your emotions and personality into a wedding.<br><br>
-
-The fairytale that you’ve always dreamt of, no matter how crazy or simple, has to be designed and put into a viable plan. We make sure everything about your fairytale screams you!</p>      
-    </div>
-  </div>
-  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
-  <div class="card mb-4">
-  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
-  <img class="card-img-top" style="width:100px;margin:30px auto;" src="<?php echo get_template_directory_uri() . '/assets_inside/respect.png' ?>" alt="Card image cap">    
-    <div class="card-body  text-center">
-      <h5 class="card-title">Fairtale</h5>
-      <p class="card-text">Constructing the Fairy Tale within a budget.<br><br>
-
-We understand that the hard-earned money being spent on such important days needs to be justified, without compromising on your wedding vision.<br><br>
-
-Working budget backwards ensures we plan and design a wedding suited to your requirements.</p>      
-    </div>
-  </div>
-  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
-  <div class="card mb-4">
-  <img class="card-img-top" style="width:100px;margin:30px auto;" src="<?php echo get_template_directory_uri() . '/assets_inside/wand.png' ?>" alt="Card image cap">  
-    <div class="card-body  text-center">
-      <h5 class="card-title">Flairtale</h5>
-      <p class="card-text">Making the dream plan come live into action!<br><br>
-
-The process of planning now comes to life and each and every aspect of making the wedding happen- and happen right- is taken care of by Team B Inc.<br><br>
-
-The priority here is to run the actual show seamlessly with adherence to timelines & the most painful attention to the finest details.</p>      
-    </div>
-  </div>
-</div>
-    </div>
-
-    <div class="planning">
-    <div class="heading">
-        <p>Phases</p>
-        <h1><span>*</span></h1>
-    </div>
-    <div class="card-deck">
-  <div class="card">
-    <img class="card-img-top" style="width:160px;margin:20px auto;"  src="https://images.squarespace-cdn.com/content/v1/5c4111e5b98a78c514801cef/1558377875221-39W4PF09WKNKL0OH8INA/ke17ZwdGBToddI8pDm48kPmrXPVZ9IZXdToIOIR3bYpZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7coMdWJheMWZGj8ZBPnfUaPSgvGrwmH9g7Am67QkkDgnFR0Pz3fYPXtHzuPoZ7T36Q/smallicons-11.png?format=300w" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center">Planning Phase</h5>
-      <p class="card-text">During this phase we meet, greet and get to know the style and vibe of our bride and groom. We get down-to-details like the budget and guest list to make sure we’re aligned for the requirements of your big day. Then, we collaborate, concept and start creating a vision. Have you always dreamed of a small, intimate backyard party or are you more of a ballroom, fairytale, where’s-my-glass slipper kind of a girl? Deciding on your dream direction allows us to choose vendors and plot key milestones to make your wedding day a reality. </p>      
-    </div>
-  </div>
-  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
-  <div class="card">
-    <img class="card-img-top" style="width:160px;margin:20px auto;"  src="https://images.squarespace-cdn.com/content/v1/5c4111e5b98a78c514801cef/1558377805267-2QR1PJ7QRQI9WOS7A52N/ke17ZwdGBToddI8pDm48kPmrXPVZ9IZXdToIOIR3bYpZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7coMdWJheMWZGj8ZBPnfUaPSgvGrwmH9g7Am67QkkDgnFR0Pz3fYPXtHzuPoZ7T36Q/smallicons-10.png?format=300w" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center">Design Phase</h5>
-      <p class="card-text">TWith the big picture nailed down in phase one, now’s the time we bring your style to life. We create a custom design proposal encompassing everything from your cake to invitations to unexpected centerpieces that will have your guests Instagramming every last thoughtfully curated detail. We’ll handle sourcing and vendor communication so you can enjoy the surprise of “reveal day” when we share mockups of your entire design suite.
-
-</p>      
-    </div>
-  </div>
-  <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
-  <div class="card">
-    <img class="card-img-top" style="width:160px;margin:20px auto;"  src="https://images.squarespace-cdn.com/content/v1/5c4111e5b98a78c514801cef/1558377895913-ET0QUOVDVBWTB9GZZSK8/ke17ZwdGBToddI8pDm48kPmrXPVZ9IZXdToIOIR3bYpZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7coMdWJheMWZGj8ZBPnfUaPSgvGrwmH9g7Am67QkkDgnFR0Pz3fYPXtHzuPoZ7T36Q/smallicons-12.png?format=300w" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center">Action Phase</h5>
-      <p class="card-text">Now, the planning pays off and your vision comes to life. Your joy takes center stage while our team carefully nuances every detail and manages any obstacle. From red wine stains to sick officiants, we take over so you and your family can embrace every moment of your big day.</p>      
-    </div>
-  </div>  
-</div>
-    </div>
+    
     <div class="meet-team">
     <div class="heading">
         <p>Meet the team</p>
@@ -407,3 +327,4 @@ The priority here is to run the actual show seamlessly with adherence to timelin
     
 </div>
 <?php get_footer(); ?>
+
