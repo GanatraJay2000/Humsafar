7c61ad7620b6d0bd3f6d6d87be106639b51d3d91
GanatraJay2000 <jbg@somaiya.edu>
2020-08-13 20:30:49 +0530

    hahaha

--- page-services.php
+++ page-services.php
@@ -37,7 +37,9 @@
             transition:1s;
             background-color:#000000aa;
             color:white;
-            font-size:18px;
+            font-size:19px;
+            display:grid;
+            place-items:center;            
             max-height:0;
             height:100%;
             width:100%;            
@@ -81,7 +83,7 @@
                          <img alt="venue"
                         src="https://images.pexels.com/photos/1035665/pexels-photo-1035665.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
                         <div class="block_content">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam error obcaecati culpa rerum blanditiis laboriosam? Nam ratione vel magni eligendi quia voluptates. Quis non numquam quod reprehenderit iusto doloribus modi impedit consectetur recusandae explicabo. Inventore eaque libero quas suscipit debitis ratione nemo, veritatis id ab assumenda quos cupiditate sit molestias!
+                            Budget planning is essential as it cuts off the extra spending and utilize the money in a planned way. But, we  can actually help you out to achieve your dreams and have a perfect wedding just according to your choice and most importantly providing you with the best according to your budget. 
                         </div>
                     </div>
                     <div class="title">Budget Management</div>
@@ -92,7 +94,7 @@
                         <img alt="venue"
                         src="https://images.pexels.com/photos/1035665/pexels-photo-1035665.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
                         <div class="block_content">
-                                    The place where you witness those life changing moments has to be as special as it can be. Planning the destination is a harder yet an important part which is a specialization of Humsafar Weddings. Is it a traditional palace like or a beach with picturesque sceneries you like, we've got you covered. We provide Pan India destination services with tip to top organization. Foreign destination you dreamt of is our responsibility too. All the planning and execution of your wedding arrangements are dealt with utmost care and professionalism.
+                                  Planning the destination is a specialization of Humsafar weddings. Is it a traditional palace like or a beach with picturesque sceneries you like, we've got you covered. We provide Pan India and foreign destination services with tip to top organization. 
                         </div>
                     </div>
                 <div class="title">Venues & Destination</div>
@@ -102,7 +104,9 @@
                         <img alt="venue"
                         src="https://images.pexels.com/photos/1035665/pexels-photo-1035665.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
                         <div class="block_content">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam error obcaecati culpa rerum blanditiis laboriosam? Nam ratione vel magni eligendi quia voluptates. Quis non numquam quod reprehenderit iusto doloribus modi impedit consectetur recusandae explicabo. Inventore eaque libero quas suscipit debitis ratione nemo, veritatis id ab assumenda quos cupiditate sit molestias!
+                         Food stations are the most important thing in any and every wedding. Selecting the right menu with respect to the season is essential to have a perfect amiable wedding.
+ We help you out with a proper planning and management of food by providing the best seasonal and traditional food according to the theme. Thus, we work to achieve perfection and to help you out in the best way we can. 
+
                         </div>
                     </div>
                 <div class="title">Catering & Menu Planning</div>
@@ -122,7 +126,9 @@
                        <img alt="venue"
                         src="https://images.pexels.com/photos/1035665/pexels-photo-1035665.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
                         <div class="block_content">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam error obcaecati culpa rerum blanditiis laboriosam? Nam ratione vel magni eligendi quia voluptates. Quis non numquam quod reprehenderit iusto doloribus modi impedit consectetur recusandae explicabo. Inventore eaque libero quas suscipit debitis ratione nemo, veritatis id ab assumenda quos cupiditate sit molestias!
+                          Is Haldi possible without turmeric? Is Sangeet possible without music?<br>
+Of course not!<br> Only a proper plan as input will deliver entertainment as an output. We can guarantee you the level of your entertainment! Finally, your day is not only a memorable day for you alone but also for many! 
+
                         </div>
                     </div>
                 <div class="title">Entertainment Design & Planning </div>
@@ -132,7 +138,8 @@
                         <img alt="venue"
                         src="https://images.pexels.com/photos/1035665/pexels-photo-1035665.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
                         <div class="block_content">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam error obcaecati culpa rerum blanditiis laboriosam? Nam ratione vel magni eligendi quia voluptates. Quis non numquam quod reprehenderit iusto doloribus modi impedit consectetur recusandae explicabo. Inventore eaque libero quas suscipit debitis ratione nemo, veritatis id ab assumenda quos cupiditate sit molestias!
+                                                   Design your dreams and live in it!<br>
+If designs are humans, all women will be taken-off by them. The matter is the visual attraction! Each place has its own beauty but decorations adore it. The Humsafar Weddings are best in it and will turn your place to a Disney World!
                         </div>
                     </div>
                 <div class="title">Design & Decor</div>
@@ -142,7 +149,9 @@
                         <img alt="venue"
                         src="https://images.pexels.com/photos/1035665/pexels-photo-1035665.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
                         <div class="block_content">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam error obcaecati culpa rerum blanditiis laboriosam? Nam ratione vel magni eligendi quia voluptates. Quis non numquam quod reprehenderit iusto doloribus modi impedit consectetur recusandae explicabo. Inventore eaque libero quas suscipit debitis ratione nemo, veritatis id ab assumenda quos cupiditate sit molestias!
+                                 Every guest is a precious jewel until treated with hospitality!
+But on your over occupied day will you be able to do that? Don’t worry. We do this with concern, because we know how special you are to us and the day is to you!
+
                         </div>
                     </div>
                 <div class="title">Hospitality & Guest Management</div>
@@ -152,7 +161,9 @@
                         <img alt="venue"
                         src="https://images.pexels.com/photos/1035665/pexels-photo-1035665.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
                         <div class="block_content">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam error obcaecati culpa rerum blanditiis laboriosam? Nam ratione vel magni eligendi quia voluptates. Quis non numquam quod reprehenderit iusto doloribus modi impedit consectetur recusandae explicabo. Inventore eaque libero quas suscipit debitis ratione nemo, veritatis id ab assumenda quos cupiditate sit molestias!
+                             A form of love and greetings with a heartiest invite to our friends, family and relatives needs to be very special.
+We help you out in designing a beautiful and attractive invitation card along with trending and exciting gifts and that too in your  budget price only. Thus, make sure that your gifts are bombarded with a bundle of excitement and thrill capturing their eyes. 
+
                         </div>
                     </div>
                 <div class="title">Invitations & Gifting </div>
@@ -162,7 +173,9 @@
                        <img alt="venue"
                         src="https://images.pexels.com/photos/1035665/pexels-photo-1035665.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
                         <div class="block_content">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam error obcaecati culpa rerum blanditiis laboriosam? Nam ratione vel magni eligendi quia voluptates. Quis non numquam quod reprehenderit iusto doloribus modi impedit consectetur recusandae explicabo. Inventore eaque libero quas suscipit debitis ratione nemo, veritatis id ab assumenda quos cupiditate sit molestias!
+                                       “You don’t take a photograph, you make it ” ~Ansel Adams<br>
+The beauty of it is, it remains constant even after time changes! Videography will take you to the journey of the moments which you might have left unobserved. Chill, for we are here to carry out your day with perfection.
+
                         </div>
                     </div>
                 <div class="title">Photography & Videography</div>
@@ -172,7 +185,7 @@
                         <img alt="venue"
                         src="https://images.pexels.com/photos/1035665/pexels-photo-1035665.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
                         <div class="block_content">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam error obcaecati culpa rerum blanditiis laboriosam? Nam ratione vel magni eligendi quia voluptates. Quis non numquam quod reprehenderit iusto doloribus modi impedit consectetur recusandae explicabo. Inventore eaque libero quas suscipit debitis ratione nemo, veritatis id ab assumenda quos cupiditate sit molestias!
+                         Planning a destination wedding is a great idea for couples but it comes with great deal of legal procedures. Humsafar weddings follows it’s legal responsibility by abiding to the rules and regulations. We have a team specializing in handling all the legal matters quickly.
                         </div>
                     </div>
                 <div class="title">Permissions & Licenses</div>
