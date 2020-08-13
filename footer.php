8944f70c38dc768630340f3ec94dd53299d3b5fd
GanatraJay2000 <jbg@somaiya.edu>
2020-08-14 01:08:28 +0530

    hehe

--- footer.php
+++ footer.php
@@ -69,7 +69,7 @@ foreach ( $slide_2 as $image ) {
             <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">                
                     <div class="modal-content">       
-                        <div class="modal-header bg-light">
+                        <div class="modal-header bg-secondary">
                         <h5 class="modal-title">Subscribe to Newsletter</h5>
                         <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
                         <span class="text-white" aria-hidden="true">&times;</span>
